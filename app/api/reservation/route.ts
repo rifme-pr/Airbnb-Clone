a9ba83268321062cd57e86e08c88db048b7fdcb0
rifat ahmed <rifatahmed0255@gmail.com>
2024-02-20 21:28:34 -0800

    'new'

--- app/api/reservation/route.ts
+++ app/api/reservation/route.ts
@@ -22,7 +22,7 @@ export async function POST(request: Request){
             id: listingId
         },
         data: {
-            reservation: {
+            reservations: {
                 create: {
                     userId: currentUser.id,
                     startDate,
